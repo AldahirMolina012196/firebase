b1e3becad62e00aa7a64c57e2d757594516e5e04
JRGONGDEV <julioalbertocano@gmail.com>
2019-08-10 10:08:49 -0500

    configuring firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBIM6XVuaqeBDtyyB9Ef1U5oUv9Cue9BK8',
+      authDomain: 'first-firebase-angular.firebaseapp.com',
+      databaseURL: 'https://first-firebase-angular.firebaseio.com',
+      projectId: 'first-firebase-angular',
+      storageBucket: 'first-firebase-angular.appspot.com',
+      messagingSenderId: '306103315077',
+    }),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
